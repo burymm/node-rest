5b8ffb8504cbafd1c7dd2e29e49e62b8df8852bf
Mikalai Bury <burymm@gmail.com>
2015-08-06 00:17:44 +0300

    - add feature to save arrays

--- routes/products.js
+++ routes/products.js
@@ -6,23 +6,31 @@ var Products = require('../models/products.js');
 router.get('/', function(req, res, next) {
 	Products.find(function (err, products) {
 		if (err) {
-			console.log("error", err);
 			return res.send(err);
 		}
 		res.json(products);
-
 	});
 });
 
 router.post('/', function(req, res, next) {
-	var product = new Products(req.body);
-	product.save(function(err) {
-		if (err) {
-			return res.send(err);
-		}
-
-		res.send({message: "Product added"})
-	});
+	if (Array.isArray && Array.isArray(req.body)) {
+		req.body.map(function(item) {
+			var product = new Products(item);
+			product.save(function(err) {
+				if (err) {
+					return res.send(err);
+				}
+			});
+		});
+	} else {
+		var product = new Products(req.body);
+		product.save(function(err) {
+			if (err) {
+				return res.send(err);
+			}
+			res.send({message: "Product added"})
+		});
+	}
 });
 
 module.exports = router;
